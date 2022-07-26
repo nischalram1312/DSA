28ecbf18aae9d86bf9a98972eab509860f09fc0c
Nischal Raghuram Puvvala <nischalram1312@gmail.com>
2022-07-26 11:47:17 +0530

    LinkedList folder added

---
+++ LinkedList/src/com/nischal/Main.java
@@ -0,0 +1,19 @@
+package com.nischal;
+
+public class Main{
+    public static void main(String[] args) {
+        LL l = new LL();
+        l.insertFirst(9);
+        l.insertFirst(10);
+        l.insertFirst(22);
+        l.insertFirst(12);
+        l.insertFirst(34);
+        l.insertFirst(29);
+        l.insertLast(66);
+        l.insert(99,2);
+        l.display();
+
+
+    }
+
+}
